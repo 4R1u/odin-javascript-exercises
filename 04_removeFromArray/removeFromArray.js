81823524b77dbe961de6e313ec0f692691358b82
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-09-28 15:02:07 +0400

    Solve Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    for (const removedItem of items) {
+        for (let removedIndex = array.indexOf(removedItem);
+             -1 !== removedIndex;
+             removedIndex = array.indexOf(removedItem)) {
+            array.splice(removedIndex, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
