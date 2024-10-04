7032d8be1a4757591b2cc334e5d6de49403e0ca1
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-04 18:42:56 +0400

    Solve Exercise 10 Without Array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -2,14 +2,17 @@ const fibonacci = function(num) {
     if (num < 0) return "OOPS";
     else if (num == 0) return 0;
     else if (num <= 2) return 1;
-    let nums = [1,1];
+    let a = 1, b = 1;
     for (let i = 0; i < num - 2; ++i) {
-        nums.push(
-            nums[nums.length - 1]
-            + nums[nums.length - 2]
-        );
+        // nums.push(
+        //     nums[nums.length - 1]
+        //     + nums[nums.length - 2]
+        // );
+        const oldB = b;
+        b = a + b;
+        a = oldB;
     }
-    return nums[num - 1];
+    return b;
 };
 
 // Do not edit below this line
