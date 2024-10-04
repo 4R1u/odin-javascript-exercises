acc5263fad8d320025393f1b21c687fe31de67e8
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-04 17:10:24 +0400

    Add factorial(num) to Exercise 8

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    for (let i = 0; i < str.length / 2; ++i) {
+        if (str[i] != str[-i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
