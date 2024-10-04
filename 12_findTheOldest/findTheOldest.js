1eb8c9b325e602ac83d8b97ecafb7fd6122b7fcb
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-04 20:07:50 +0400

    Attempt Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ages = people.map((person) => 
+        (("yearOfDeath" in person)
+        ? person.yearOfDeath
+        : new Date().getFullYear()) 
+        - person.yearOfBirth
+    );
+    const oldestAge = Math.max(...ages);
+    console.log(ages);
+    console.log(oldestAge);
+    return people[ages.indexOf(oldestAge)];
 };
 
 // Do not edit below this line
