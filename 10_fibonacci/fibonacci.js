dd30ad50664d19d92ca8a6dd12b7623456d3f9db
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-04 18:39:41 +0400

    Solve Exercise 10 Wastefully

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    else if (num == 0) return 0;
+    else if (num <= 2) return 1;
+    let nums = [1,1];
+    for (let i = 0; i < num - 2; ++i) {
+        nums.push(
+            nums[nums.length - 1]
+            + nums[nums.length - 2]
+        );
+    }
+    return nums[num - 1];
 };
 
 // Do not edit below this line
